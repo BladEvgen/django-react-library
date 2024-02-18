6c61903ee78cc11d346c3205ac1af88cd44c6b0d
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-02-18 23:07:01 +0600

    api changed
    Now avaible to check all categories and authors, also query options to filter data on page

--- back/django_app/views.py
+++ back/django_app/views.py
@@ -1,15 +1,28 @@
+import json
+
 from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.request import Request
 from rest_framework.response import Response
-import time
-from django_app.models import Book
+
+from django_app import models
 
 
 @api_view(["GET"])
 def get_books(request: Request) -> Response:
-    queryset = Book.objects.all()
+    queryset = models.Book.objects.all()
+    category_slug = request.GET.get("category")
+    if category_slug:
+        category = get_object_or_404(models.Category, slug=category_slug)
+        queryset = queryset.filter(categories=category)
+    author_slug = request.GET.get("author")
+    if author_slug:
+        author = get_object_or_404(models.Author, slug=author_slug)
+        queryset = queryset.filter(authors=author)
     selected_page = request.GET.get(key="page", default=1)
     pages = Paginator(object_list=queryset, per_page=2)
     page = pages.page(number=selected_page)
@@ -19,14 +32,60 @@ def get_books(request: Request) -> Response:
             "id": x.id,
             "title": x.title,
             "description": x.description,
-            "book_file": (
-                request.build_absolute_uri(x.book_file.url) if x.book_file else None
+            "book_image": (
+                request.build_absolute_uri(x.book_image.url) if x.book_image else None
             ),
+            "categories": [category.name for category in x.categories.all()],
+            "author_name": [author.name for author in x.authors.all()],
         }
         for x in page.object_list
     ]
     total_count = len(queryset)
-    time.sleep(1)
+    return Response(
+        {
+            "data": serializer_data,
+            "total_count": total_count,
+            "status": status.HTTP_200_OK,
+        }
+    )
+
+
+@api_view(["GET"])
+def get_categories(request: Request) -> Response:
+    categories = models.Category.objects.all()
+    serializer_data = [
+        {"categories_title": category.name, "categories_slug": category.slug}
+        for category in categories
+    ]
+    total_count = len(categories)
     return Response({"data": serializer_data, "total_count": total_count})
 
-    # "book_file": x.book_file
+
+@api_view(["GET"])
+def get_authors(request: Request) -> Response:
+    authors = models.Author.objects.all()
+    serializer_data = [
+        {"author_name": author.name, "author_slug": author.slug} for author in authors
+    ]
+    total_count = len(authors)
+    return Response({"data": serializer_data, "total_count": total_count})
+
+
+@csrf_exempt
+@require_http_methods(["POST"])
+def create_book(request: Request) -> Response:
+    data = json.loads(request.body)
+    book_data = data.get("bookData", [{}])[0]
+    title = book_data.get("title", "")
+    description = book_data.get("description", "")
+    categories = book_data.get("categories", [])
+    book_image = book_data.get("book_image", None)
+
+    book = models.Book(title=title, description=description)
+    book.book_file.save(book_image.name, book_image)
+    book.save()
+
+    for category_name in categories:
+        category, created = models.Category.objects.get_or_create(name=category_name)
+        book.categories.add(category)
+    return Response({"status": status.HTTP_200_OK})

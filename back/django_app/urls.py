24491c5484043897e26dce7663ec82e07db50b68
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-02-18 23:05:01 +0600

    new path for api, also for debug allow show img

--- back/django_app/urls.py
+++ back/django_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from django_app import views
 
 urlpatterns = [
-    path('api/book/', views.get_books, name='book-list'),
+    path("api/book/", views.get_books, name="book-list"),
+    path("api/categories/", views.get_categories, name="categories-list"),
+    path("api/authors/", views.get_authors, name="authors-list"),
 ]
